65d9baadf67f236635a6f558592fec1ffeb916c4
Jzdoxc <Jzdoxc@outlook.com>
2018-05-19 16:03:15 +0800

    视图函数操作数据库

--- hello.py
+++ hello.py
@@ -1,15 +1,24 @@
-from flask import Flask
+from flask import Flask,render_template,session,redirect,url_for,flash
+from flask_script import Manager
+from flask_bootstrap import Bootstrap
+from flask_wtf import Form
+from wtforms import StringField,SubmitField
+from wtforms.validators import Required
 from flask_sqlalchemy import SQLAlchemy
-from flask_script import  Manager
+
 import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
-manager = Manager(app)
+
+app.config['SECRET_KEY'] = 'hard to guess string'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=True
+
+manager = Manager(app)
+bootstrap = Bootstrap(app)
 db = SQLAlchemy(app)
 
 
@@ -17,7 +26,7 @@ class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
-    users = db.relationship('User', backref='role')
+    users = db.relationship('User', backref='role',lazy='dynamic')
 
     def __repr__(self):
         return '<Role %r>' % self.name
@@ -31,6 +40,34 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User %r>' % self.username
+
+
+class NameForm(Form):
+    name = StringField('What is your name', validators=[Required()])
+    submit = SubmitField('Submit')
+
+
+
+@app.route('/',methods=['GET','POST'])
+def index():
+    form=NameForm()
+    if form.validate_on_submit():
+        user=User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user=User(username=form.name.data)
+            db.session.add(user)
+            session['known']=False
+        else:
+            session['known']=True
+        session['name']=form.name.data
+        form.name.data=''
+        return redirect(url_for('index'))
+    return render_template('index.html',form=form,name=session.get('name'),known=session.get('known',False))
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'),404
+
 
 if __name__=='__main__':
     manager.run()
