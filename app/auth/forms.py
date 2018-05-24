a455c0fe0fd92bf7a564ce39df1ba506320bb6bd
jzdoxc <jzdoxc@outlook.com>
2018-05-24 18:23:22 +0800

    注册用户

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,9 +1,27 @@
-from flask_wtf import  Form
-from wtforms import StringField,PasswordField,BooleanField,SubmitField
-from wtforms.validators import  Required,Length,Email,DataRequired
+from flask_wtf import Form
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import Required, Length, Email, DataRequired, Regexp,EqualTo
+from ..models import User
 
 class LoginForm(Form):
-    email = StringField('Email',validators=[DataRequired(),Length(1,64),Email()])
-    password=PasswordField('Password',validators=[DataRequired()])
-    remember_me=BooleanField('Keep me logged in')
+    email = StringField('Email', validators=[DataRequired(), Length(1, 64), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Keep me logged in')
     submit = SubmitField('Log In')
+
+
+class RegistrationForm(Form):
+    email = StringField('Email', validators=[DataRequired(), Length(1, 64), Email()])
+    username = StringField('Username', validators=[DataRequired(), Length(1, 64),
+                                                   Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0, '用户名必须包含数字，字母，或下划线')])
+    password = PasswordField('Password', validators=[DataRequired(),EqualTo('password2',message='两次密码必须一致')])
+    password2 = PasswordField('Confirm Password',validators=[DataRequired()])
+    submit = SubmitField('Register')
+
+    def validate_email(self,field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValueError('该邮箱已经被注册了')
+
+    def validate_username(self,field):
+        if User.query.filter_by(username=field.data).first():
+            raise ValueError('该用户名已经被注册')
--- app/auth/views.py
+++ app/auth/views.py
@@ -1,28 +1,41 @@
-from flask import render_template, redirect, url_for,request,flash
+from flask import render_template, redirect, url_for, request, flash
 from flask_login import login_required, login_user, logout_user
 from . import auth
-from ..models import  User
-from .forms import  LoginForm
+from ..models import User, db
+from .forms import LoginForm, RegistrationForm
 
-@auth.route('/login',methods=['GET', 'POST'])
+
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        user=User.query.filter_by(email=form.email.data).first()
+        user = User.query.filter_by(email=form.email.data).first()
         if user is not None and user.verify_password(form.password.data):
-            login_user(user,form.remember_me.data)
-            next=request.args.get('next')
-            if  next is None or not next.startwith('/'):
-                next=url_for('main.index')
+            login_user(user, form.remember_me.data)
+            next = request.args.get('next')
+            if next is None or not next.startwith('/'):
+                next = url_for('main.index')
             return redirect(next)
         flash('无效的用户名或密码')
-    return render_template('auth/login.html',form=form)
+    return render_template('auth/login.html', form=form)
+
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email=form.email.data, username=form.username.data, password=form.password.data)
+        db.session.add(user)
+        flash("你已经可以登陆了")
+        return redirect(url_for('auth.login'))
+    return render_template('auth/register.html', form=form)
 
 
 @auth.route('/secret')
 @login_required
 def secret():
     return 'Only authenticated users are allowed'
+
 
 @auth.route('/logout')
 @login_required
