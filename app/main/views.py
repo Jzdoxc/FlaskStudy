a22d08a577802de0637959eb3da740ded921b78a
jzdoxc <jzdoxc@outlook.com>
2018-06-16 21:01:03 +0800

    博客列表

--- app/main/views.py
+++ app/main/views.py
@@ -4,27 +4,20 @@ from flask_login import login_required, current_user
 from app import db
 from app.decorators import admin_required, permission_required
 from . import main
-from ..models import User, Permission
-from .forms import EditProfileForm, EditProfileAdminForm
+from ..models import User, Permission, Post
+from .forms import EditProfileForm, EditProfileAdminForm, PostForm
 
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
-    # form=NameForm()
-    # if form.validate_on_submit():
-    #     user=User.query.filter_by(username=form.name.data).first()
-    #     if user is None:
-    #         user=User(username=form.name.data)
-    #         db.session.add(user)
-    #         session['known']=False
-    #         if current_app.config['FLASKY_ADMIN']:
-    #             send_mail(main.config['FLASKY_ADMIN'],'新的用户','mail/new_user',user=user)
-    #     else:
-    #         session['known']=True
-    #     session['name']=form.name.data
-    #     form.name.data=''
-    #     return redirect(url_for('.index'))
-    return render_template('index.html', name=session.get('name'), current_time=datetime.utcnow())
+    form = PostForm()
+    if current_user.can(Permission.WRITE_ARTICLES) and \
+            form.validate_on_submit():
+        post = Post(body=form.body.data, author=current_user._get_current_object())
+        db.session.add(post)
+        return redirect(url_for('.index'))
+    posts = Post.query.order_by(Post.timestamp.desc()).all()
+    return render_template('index.html', form=form, posts=posts)
 
 
 @main.route('/admin')
